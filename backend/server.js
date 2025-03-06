a6acd6241c49cf30ee689757797a0b813b371d98
Duc Nguyen <howtodrawapicture@gmail.com>
2025-03-06 14:10:10 -0600

    added boilerplate for api key

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
-const http = require('http');
+const express = require("express");
 
 const PORT = 3001;
 
-const server = http.createServer((req, res) => {
-  res.end('Hello from Node server!');
+const API_KEY = "";
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=29.7604&lon=-95.3698&appid=${API_KEY}&units=imperial`;
+
+const app = express();
+app.use(express.json());
+
+app.use();
+
+app.use("/", (req, res) => {
+  res.send("Welcome to the server :)");
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
